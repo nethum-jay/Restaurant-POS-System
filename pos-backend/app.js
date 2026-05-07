eb627aa799ad24db5d774ae57a2bf9028db55746
nethum-jay <njayanuka@gmail.com>
2026-05-07 21:18:53 +0530

    Updated POS system and database configuration

--- pos-backend/app.js
+++ pos-backend/app.js
@@ -1,17 +1,28 @@
-require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 const app = express();
 
 
-const PORT = process.env.PORT;
+const PORT = config.port;
 connectDB();
 
+// Root Endpoint
 app.get("/", (req,res) => {
+
+    const err = createHttpError(404, "Something went wrong!");
+    throw err;
+    
     res.json({message : "Hello from POS Server!"});
 }) 
 
+// Global Error Handler
+app.use(globalErrorHandler);
 
+
+//Server 
 app.listen(PORT, () => {
-    console.log(`POS Server is listening on port ${PORT}`);
+    console.log(` ☑️ POS Server is listening on port ${PORT}`);
 })
