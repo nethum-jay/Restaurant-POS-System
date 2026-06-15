fca21c4bdd015d27a0ab0b3da7203a5155a3be30
nethum-jay <njayanuka@gmail.com>
2026-06-15 19:03:34 +0530

    Added Login and Register components for authentication

--- pos-frontend/src/pages/Auth.jsx
+++ pos-frontend/src/pages/Auth.jsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import restaurant from "../assets/images/restaurant-img.jpg"
 import logo from "../assets/images/logo.png"
+import Register from "../components/auth/Register"
+import Login from "../components/auth/Login"
 
 const Auth = () => {
+
+  const [isRegister, setIsRegister] = useState(false);
+
   return (
-    <div className="flex -min-h-screen w-full">
+    <div className="flex min-h-screen w-full">
       {/* Left Selection */}
-      <div className="w1/2 relative flex items-center justify-center bg-cover">
+      <div className="w-1/2 relative flex items-center justify-center bg-cover">
         {/* BG Image */}
         <img className="w-full h-full object-cover"src={restaurant} alt="Restaurant Image" />
 
@@ -15,7 +20,7 @@ const Auth = () => {
 
         {/* Quote at bottom */}
         <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-white">
-          "Serve customers the best with prompt and friendly service in a 
+          "Serve customers the best food with prompt and friendly service in a 
           welcoming atmosphere, and they'll keep coming back."
           <br />
           <span className="block mt-4 text-yellow-400">- Founder of Restro</span>
@@ -28,6 +33,25 @@ const Auth = () => {
           <img src={logo} alt="Restro Logo" className="h-14 w-14 border-2 rounded-full p-1" />
           <h1 className="text-lg font-semibold text-[#f5f5f5] tracking-wide">Restro</h1>
         </div>
+
+        <h2 className="text-4xl text-center mt-10 font-semibold text-yellow-400 mb-10">
+          {isRegister ? "Employee Registration" : "Employee Login"}
+        </h2>
+
+        {/* Component */}
+        {isRegister ? <Register /> : <Login />}
+
+
+        <div className="flex justify-center mt-6">
+          <p className="text-sm text-[#ababab]">
+            {isRegister ? "Already have an account?" : "Don't have an account?"}
+              <a onClick={() => setIsRegister(!isRegister)} className="text-yellow-400 font-semibold hover:underline" href="#">
+                {isRegister ? "Singn in" : "Singn up"}
+              </a>
+          </p>
+
+        </div>
+
       </div>
     </div>
   )
