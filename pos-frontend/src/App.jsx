4e914ab23a8a1dabeee652d22809a20743840f68
nethum-jay <njayanuka@gmail.com>
2026-06-15 07:39:52 +0530

    Fixed Tailwind CSS typos and improved Auth page UI

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -1,24 +1,37 @@
-
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Home, Auth, Orders, Tables, Menu } from "./pages";
-import Header from "./components/shared/Header.jsx";
+import Header from "./components/shared/Header";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/auth"];
+
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
+    <> 
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>      
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/orders" element={<Orders />} />
           <Route path="/tables" element={<Tables />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
-      </Router>
+      </Routes>
     </>
   );
+}
+  
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  )
 }
 
 export default App;
--- pos-frontend/src/pages/Auth.jsx
+++ pos-frontend/src/pages/Auth.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from "react"
+import restaurant from "../assets/images/restaurant-img.jpg"
+import logo from "../assets/images/logo.png"
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className="flex -min-h-screen w-full">
+      {/* Left Selection */}
+      <div className="w1/2 relative flex items-center justify-center bg-cover">
+        {/* BG Image */}
+        <img className="w-full h-full object-cover"src={restaurant} alt="Restaurant Image" />
+
+        {/* Black Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+
+        {/* Quote at bottom */}
+        <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-white">
+          "Serve customers the best with prompt and friendly service in a 
+          welcoming atmosphere, and they'll keep coming back."
+          <br />
+          <span className="block mt-4 text-yellow-400">- Founder of Restro</span>
+        </blockquote>
+      </div>
+
+      {/* Right Selection */}
+      <div className="w-1/2 min-h-screen bg-[#1a1a1a] p-10">
+        <div className="flex flex-col items-center gap-2">
+          <img src={logo} alt="Restro Logo" className="h-14 w-14 border-2 rounded-full p-1" />
+          <h1 className="text-lg font-semibold text-[#f5f5f5] tracking-wide">Restro</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
